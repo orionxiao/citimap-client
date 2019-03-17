f9968446974a2f1e9fd9eff786cd28f9d2eaceed
Orion Xiao <opx200@stern.nyu.edu>
2019-03-16 22:59:27 -0400

    more documentation and marking todos

--- src/Components/App.js
+++ src/Components/App.js
@@ -48,8 +48,9 @@ class App extends Component {
         stations.map(s =>
             markers.push(
                 L.marker([s.coords.lat, s.coords.long], {
-                    icon: this.getIcon(s.bikes)
-                }).bindPopup(`${s.name} Bikes: ${s.bikes}`)
+                    icon: this.getIcon(s.bikes),
+                    title: s.name
+                }).bindPopup(`Bikes: ${s.bikes}`)
             )
         );
 
@@ -95,24 +96,27 @@ class App extends Component {
         console.log(lat, long);
         const resp = await fetch(
             `http://localhost:3001/api/test/${lat}/${long}`
-        );
+        ); //API call
         const reader = resp.body.getReader();
-        let buf = "";
-        let self = this;
-
+        let buf = ""; //Store string data from unsigned int conversion
+        let self = this; //TODO: make this part look less hacky by binding the createMap function or something
+        /**
+         * Recursively process chunks of unsigned int arrays from ReadableStream and convert
+         * back into stations object
+         */
         reader.read().then(function processChunk({ done, value }) {
             if (done) {
                 console.log("Stream complete");
-                const stations = JSON.parse(buf);
+                const stations = JSON.parse(buf); //Parse result
                 console.log(stations);
                 console.log(stations.length);
                 self.createMap(stations);
                 return;
             } else {
                 console.log("chunk");
-                let str = new TextDecoder("utf-8").decode(value);
-                buf = buf.concat(str);
-                return reader.read().then(processChunk);
+                let str = new TextDecoder("utf-8").decode(value); //Convert from uint8array into string
+                buf = buf.concat(str); //Add to prior chunks
+                return reader.read().then(processChunk); //Recurse until done
             }
         });
     };
